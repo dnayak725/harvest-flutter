0986e2682e6c4b453a48f80eca8c79d9e313af3b
Nayak <82202750+dnayak725@users.noreply.github.com>
2022-02-23 13:02:35 +0530

    update

--- lib/Screens/AddCrop.dart
+++ lib/Screens/AddCrop.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:havest/Screens/FarmerDetails.dart';
 
+
 class AddCrop extends StatefulWidget {
   const AddCrop({Key? key}) : super(key: key);
 
